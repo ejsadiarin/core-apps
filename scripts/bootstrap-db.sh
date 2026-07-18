f948e3cfbc0a113cc1c42f8ad996020f09d2206b
ejsadiarin <ejs@ejsadiarin.com>
2026-07-18 17:39:29 +0800

    chore: add sqlc generate

--- scripts/bootstrap-db.sh
+++ scripts/bootstrap-db.sh
@@ -2,9 +2,6 @@
 
 DB_URL="${DATABASE_URL:?DATABASE_URL is required}"
 
-# create schema
-# run migrations & sqlc
-
 # NOTE: Run this script at root
 declare -A schema_services_map=(
     ["coregateway"]="./db/migrations"
@@ -12,6 +9,8 @@ declare -A schema_services_map=(
     ["coreban"]="./services/coreban/db/migrations"
 )
 
+# create schema
+# run goose migrations
 for schema in "${!schema_services_map[@]}"; do
     echo "-------------------------------------"
     echo "Creating schema: $schema"
@@ -19,4 +18,6 @@ for schema in "${!schema_services_map[@]}"; do
     psql "$DB_URL" -c "CREATE SCHEMA IF NOT EXISTS $schema;"
     goose -dir "${schema_services_map["$schema"]}" -table "$schema".goose_db_version postgres "$DB_URL" status
 done
-#
+
+# run sqlc
+sqlc generate
